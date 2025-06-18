dba2cdd6d3f3aed7d3cd4a0b303bbdcd426faa31
Olga <olga_bezrukova@bk.ru>
2025-06-18 21:33:04 +0300

    feat(slider): added a slider to the layout

--- src/components/sectionNewRecipies/FlexContainer.tsx
+++ src/components/sectionNewRecipies/FlexContainer.tsx
@@ -1,55 +1,74 @@
-import { ChevronLeftIcon, ChevronRightIcon, useBreakpointValue } from '@chakra-ui/icons';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { useBreakpointValue } from '@chakra-ui/icons';
 import { Box, Flex, IconButton } from '@chakra-ui/react';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import recipesData from '../../data/recipes-data.json';
 import RecipeCard from '../customCard/RecipeCardWithPic';
 
 const RecipesGrid = () => {
     const newRecipes = recipesData.newRecipies;
-    const hideIconButton = useBreakpointValue({ sm: false, '2xl': true });
+    const isDesktop = useBreakpointValue({ base: false, xl: true });
+    const spaceBetween = useBreakpointValue({ base: 12, xl: 24 });
 
     return (
         <Box width='100%'>
             <Box position='relative'>
-                {hideIconButton && (
-                    <IconButton
-                        aria-label='Предыдущий слайд'
-                        icon={<ChevronLeftIcon boxSize={6} color='white' />}
-                        position='absolute'
-                        left='-10px'
-                        top='50%'
-                        transform='translateY(-50%)'
-                        borderRadius='4px'
-                        size='md'
-                        bg='black'
-                        colorScheme='blackAlpha'
-                        zIndex={1}
-                        _hover={{ bg: 'black' }}
-                    />
-                )}
-
-                <Flex overflowX='auto' gap={{ '3xl': '24px', sm: '12px' }}>
+                <Swiper
+                    modules={[Navigation]}
+                    spaceBetween={spaceBetween}
+                    slidesPerView={4}
+                    loop={true}
+                    touchEventsTarget={isDesktop ? 'container' : 'wrapper'}
+                    allowTouchMove={!isDesktop}
+                    navigation={
+                        isDesktop
+                            ? {
+                                  nextEl: '.swiper-button-next',
+                                  prevEl: '.swiper-button-prev',
+                              }
+                            : false
+                    }
+                >
                     {newRecipes.map((recipe) => (
-                        <Flex key={recipe.id} flex='1 1 auto' alignItems='stretch'>
-                            <RecipeCard {...recipe} />
-                        </Flex>
+                        <SwiperSlide key={recipe.id} style={{ height: 'auto' }}>
+                            <Flex px={1} height='100%'>
+                                <RecipeCard {...recipe} />
+                            </Flex>
+                        </SwiperSlide>
                     ))}
-                </Flex>
-                {hideIconButton && (
-                    <IconButton
-                        aria-label='Следующий слайд'
-                        icon={<ChevronRightIcon boxSize={6} color='white' />}
-                        position='absolute'
-                        right='-10px'
-                        top='50%'
-                        transform='translateY(-50%)'
-                        borderRadius='4px'
-                        size='md'
-                        bg='black'
-                        colorScheme='blackAlpha'
-                        zIndex={1}
-                        _hover={{ bg: 'black' }}
-                    />
+                    <style>{`
+                        .swiper-button-next,
+                        .swiper-button-prev {
+                            background: black;
+                            color: white;
+                            width: 40px;
+                            height: 40px;
+                            border-radius: 4px;
+                            position: absolute;
+                            top: 40%;  
+                        }
+                            .swiper-button-prev {
+                                left: -10px; 
+                        }
+                                 .swiper-button-next {
+                            right: -10px; 
+                        }
+                        .swiper-button-next:after,
+                        .swiper-button-prev:after {
+                            font-size: 20px;
+                        }
+                    `}</style>
+                </Swiper>
+
+                {isDesktop && (
+                    <>
+                        <IconButton className='swiper-button-prev' aria-label='Предыдущий слайд' />
+                        <IconButton className='swiper-button-next' aria-label='Следующий слайд' />
+                    </>
                 )}
             </Box>
         </Box>
