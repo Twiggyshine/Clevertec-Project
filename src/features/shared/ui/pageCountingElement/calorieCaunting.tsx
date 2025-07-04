7d664598acd39bf1c7baf2c0df8a39a3d1fc7c3d
Olga <olga_bezrukova@bk.ru>
2025-07-04 18:19:16 +0300

    feat(ui): add new CaloriesComponent with dynamic calculation

---
+++ src/features/shared/ui/pageCountingElement/calorieCaunting.tsx
@@ -0,0 +1,8 @@
+import { Flex } from '@chakra-ui/react';
+
+const CalElement = () => (
+    <Flex w='149px' h='139px' flexDirection='column' border='1px solid rgba(0, 0, 0, 0.48)'>
+        <Flex></Flex>
+    </Flex>
+);
+export default CalElement;
--- src/features/shared/ui/recipeActionBar/recipeActions.tsx
+++ src/features/shared/ui/recipeActionBar/recipeActions.tsx
@@ -3,9 +3,16 @@ import { Badge, Button, Flex, Image, Text } from '@chakra-ui/react';
 import { CustomHeartIcon, CustomSmileIcon } from '~/assets/customIcon/CustomIcon';
 
 const RecipeActions = () => (
-    <Flex gap='12px' mt='auto' justifyContent='center'>
+    <Flex
+        gap='12px'
+        justifyContent='center'
+        flexWrap='wrap'
+        flexDirection={{ base: 'column', lg: 'row' }}
+        alignItems='center'
+    >
         <Badge
-            backgroundColor='rgba(0, 0, 0, 0.06);'
+            order={{ base: 1, lg: 0 }}
+            backgroundColor='rgba(0, 0, 0, 0.06)'
             fontSize='14px'
             borderRadius='4px'
             p='2px 8px'
@@ -19,33 +26,37 @@ const RecipeActions = () => (
                 <Text>20 минут</Text>
             </Flex>
         </Badge>
-        <Button
-            bg='rgba(255, 255, 255, 0.06)'
-            border='1px solid rgba(0, 0, 0, 0.48)'
-            borderRadius='6px'
-            fontWeight='600'
-            gap='3px'
-            h='48px'
-            w='219px'
-            p='0px 24px'
-            _hover={{ bg: 'gray.700' }}
-        >
-            <CustomSmileIcon width='14px' height='14px' />
-            Оценить рецепт
-        </Button>
-        <Button
-            bg='#b1ff2e'
-            color='white'
-            p='0 12px'
-            _hover={{ bg: 'gray.700' }}
-            borderRadius='6px'
-            w='273px'
-            h='48px'
-            fontWeight='600'
-        >
-            <CustomHeartIcon width='14px' height='14px' />
-            Сохранить в закладки
-        </Button>
+
+        <Flex gap='12px' order={{ base: 2, lg: 0 }} justifyContent='center'>
+            <Button
+                bg='rgba(255, 255, 255, 0.06)'
+                border='1px solid rgba(0, 0, 0, 0.48)'
+                borderRadius='6px'
+                fontWeight='600'
+                gap='3px'
+                h={{ '2xl': '48px', xl: '32px', base: '24px' }}
+                w={{ '2xl': '219px', xl: '160px', base: '132px' }}
+                p='0px 24px'
+                _hover={{ bg: 'gray.700' }}
+            >
+                <CustomSmileIcon width='14px' height='14px' />
+                Оценить рецепт
+            </Button>
+            <Button
+                bg='#b1ff2e'
+                color='white'
+                p='0 12px'
+                _hover={{ bg: 'gray.700' }}
+                borderRadius='6px'
+                h={{ '2xl': '48px', xl: '32px', base: '24px' }}
+                w={{ '2xl': '273px', xl: '202px', base: '168px' }}
+                fontWeight='600'
+            >
+                <CustomHeartIcon width='14px' height='14px' />
+                Сохранить в закладки
+            </Button>
+        </Flex>
     </Flex>
 );
+
 export default RecipeActions;
