af84bedda93005a27260373d912a4baea2566e0f
Olga <olga_bezrukova@bk.ru>
2025-11-26 18:57:55 +0300

    feat(allergens): add allergens drawer

--- src/features/shared/layouts/nav/SearchInput.tsx
+++ src/features/shared/layouts/nav/SearchInput.tsx
@@ -2,31 +2,6 @@ import { Button, Flex, Input, InputGroup } from '@chakra-ui/react';
 
 import AddButton from '~/assets/icons/AddButton.svg';
 
-// export function SearchInput() {
-//     return (
-//         <Flex p='10px 10px 10px 24px' gap='8px'>
-//             <InputGroup>
-//                 <Input
-//                     type='text'
-//                     placeholder='Поиск...'
-//                     pl='10'
-//                     color=' #134b00'
-//                     id='allergen-search-input'
-//                 />
-//             </InputGroup>
-//             <Button
-//                 bg='transparent'
-//                 p='0'
-//                 display='flex'
-//                 alignItems='center'
-//                 justifyContent='center'
-//             >
-//                 <img src={AddButton} style={{ width: '20px', height: '20px' }} />
-//             </Button>
-//         </Flex>
-//     );
-// }
-
 interface Props {
     value: string;
     onChange: (v: string) => void;
